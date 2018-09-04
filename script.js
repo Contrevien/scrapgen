a1d8421b8c4c7d74fdb5d01bbf821828f102fc5a
Akhand Mishra <akkimysite@gmail.com>
2018-09-04 20:20:26 +0530

    Minor changes

--- script.js
+++ script.js
@@ -1,90 +1,78 @@
-var input = document.getElementById("url-input");
-var state = false;
-var isLoading = false;
+const input = document.getElementById("url-input");
+let state = false;
+let isLoading = false;
+const displayArea = document.getElementById("display-area");
+const loader = document.getElementById("loader-container");
 
-input.oninput = () => {
-    var url = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
-    if (url.test(input.value)===false){
-        document.getElementById("status").style.visibility = "visible";
-        state = false;
-    }
-    else {
-        document.getElementById("status").style.visibility = "hidden";
-        state = true;
-    }
-}
+const pythonEditor = CodeMirror(document.getElementById("generation-area"), {
+    value: "import selenium.webdriver from selenium\n",
+    mode: "python"
+});
 
-input.addEventListener("keydown", (e) => {
-    if (e.keyCode === 13 && state) {
-        isLoading = true;
-        document.getElementById("loader-container").style.display = "flex";
-    }
-})
-
-var buttonsText = [
+let buttonsText = [
     {
-        "div":[
-            "id=\"main\"",
-        ]
-    },
-    {
-        "h1":[
-            "id=\"header\"",
-            "My website"
-        ]
-    },
-    {
-        "img":[
-            "src=\"somewhere.png\"",
-            "alt=\"Logo\""
-        ]
-    },
-    {
-        "input":[
-            "type=\"text\"",
-            "placeholder=\"Enter Name\"",
-        ]
-    },
-    {
-        "p":[
-            "id=\"para-1\"",
-            "class=\"site-text\"",
-            "how is the weather so good today and some garbage text must go here!"
+        "article":[
+            "id='dummy'"
         ]
     }
 ];
 
-var displayArea = document.getElementById("display-area");
-
-for (var i = 0; i < buttonsText.length; i++) {
-    var newButton = document.createElement("div");
-    newButton.setAttribute("class", "button");
-    
-    var buttonHead = document.createElement("h2");
-    buttonHead.setAttribute("class", "button-head");
-    buttonHead.innerHTML = Object.keys(buttonsText[i]);
-    newButton.appendChild(buttonHead);
-
-    var buttonParaCon = document.createElement("div");
-    buttonParaCon.setAttribute("class","button-para-container");
-    for (var j in buttonsText[i][Object.keys(buttonsText[i])]){
-        var buttonPara = document.createElement("p");
-        buttonPara.setAttribute("class", "button-para");
-        buttonPara.innerHTML = buttonsText[i][Object.keys(buttonsText[i])][j].slice(0,35);
-        buttonParaCon.appendChild(buttonPara);
+const checkURL = (url) => {
+    var urlPat = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
+    if (urlPat.test(url)===false){
+        document.getElementById("status").style.visibility = "visible";
+        state = false;
+        return false;
     }
-    newButton.appendChild(buttonParaCon);
-    displayArea.appendChild(newButton);
-    newButton.addEventListener("click", select)
+    else {
+        document.getElementById("status").style.visibility = "hidden";
+        state = true;
+        return true;
+    }
+}
+
+const submitURL = (e) => {
+    if (e.keyCode === 13 && state) {
+        isLoading = true;
+        loader.style.display = "flex";
+        renderButtons(buttonsText);
+    }
 }
 
 
-var pythonEditor = CodeMirror(document.getElementById("generation-area"), {
-    value: "import selenium.webdriver from selenium\n",
-    mode: "python"
-})
+input.addEventListener("input", () => checkURL(input.value));
+input.addEventListener("keydown", submitURL);
 
-function select(e) {
+
+
+function renderButtons(buttonsText){
+    for (var i = 0; i < buttonsText.length; i++) {
+        var newButton = document.createElement("div");
+        newButton.setAttribute("class", "button");
+        
+        var buttonHead = document.createElement("h2");
+        buttonHead.setAttribute("class", "button-head");
+        buttonHead.innerHTML = Object.keys(buttonsText[i]);
+        newButton.appendChild(buttonHead);
+
+        var buttonParaCon = document.createElement("div");
+        buttonParaCon.setAttribute("class","button-para-container");
+        for (var j in buttonsText[i][Object.keys(buttonsText[i])]){
+            var buttonPara = document.createElement("p");
+            buttonPara.setAttribute("class", "button-para");
+            buttonPara.innerHTML = buttonsText[i][Object.keys(buttonsText[i])][j].slice(0,35);
+            buttonParaCon.appendChild(buttonPara);
+        }
+        newButton.appendChild(buttonParaCon);
+        displayArea.appendChild(newButton);
+        newButton.addEventListener("click", selectButton);
+        if (isLoading)
+            loader.style.display = "none";
+    }
+}
+
+
+function selectButton(e) {
     var target = null;
     if(e.target.parentNode.className === "button"||e.target.parentNode.className === "button selected"){
         target = e.target.parentNode;
